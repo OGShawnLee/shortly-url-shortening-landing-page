da090d5f8059e2247706cec0cca3cfd1717d3811
OGShawnLee <changoloco.evg@gmail.com>
2022-06-01 20:21:09 +0000

    :lipstick: fix: align feature card icon properly on desktop view

--- src/components/CardFeature.svelte
+++ src/components/CardFeature.svelte
@@ -6,7 +6,7 @@
 
 <article class="z-10 p-10 pt-18 pb-12 | bg-white rounded-lg transform {translate}">
   <div
-    class="w-20 min-w-20 h-20 min-h-20 absolute bottom-full right-1/2 | grid place-content-center | bg-violet-700 rounded-full transform translate-y-1/2 translate-x-1/2 lg:(right-full translate-x-24)">
+    class="w-20 min-w-20 h-20 min-h-20 absolute bottom-full right-1/2 | grid place-content-center | bg-violet-700 rounded-full transform translate-y-1/2 translate-x-1/2 lg:(right-full translate-x-30)">
     <img src={icon} alt="" />
   </div>
   <div class="flex flex-col gap-4 text-center lg:text-left">
